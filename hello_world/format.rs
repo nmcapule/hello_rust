2a7adc0188a732cbe18d5296b702482e0ca63fee
Nathaniel M. Capule <nmcapule@gmail.com>
2018-01-06 03:06:32 +0800

    Fooorrmat pi!

--- hello_world/format.rs
+++ hello_world/format.rs
@@ -27,4 +27,7 @@ fn main() {
     // handling. This will not work.
     // println!("This struct `{}` won't print...", Structure(3));
     // FIXME ^ Comment out this line.
+
+    let pi = 3.141592;
+    println!("Pi is roughly {:.3}", pi);
 }
