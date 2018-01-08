326dea0a2256616b530989d1bd7adde854512a5d
Nathaniel M. Capule <nmcapule@gmail.com>
2018-01-09 02:49:06 +0800

    Better roguelike

--- roguelike/src/main.rs
+++ roguelike/src/main.rs
@@ -2,6 +2,7 @@ extern crate tcod;
 
 use tcod::console::*;
 use tcod::colors;
+use tcod::colors::Color;
 use tcod::input::Key;
 use tcod::input::KeyCode::*;
 
@@ -9,7 +10,39 @@ const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 const LIMIT_FPS: i32 = 20;
 
-fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool {
+struct Object {
+    x: i32,
+    y: i32,
+    char: char,
+    color: Color,
+}
+
+impl Object {
+    pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
+        Object {
+            x: x,
+            y: y,
+            char: char,
+            color: color,
+        }
+    }
+
+    pub fn move_by(&mut self, dx: i32, dy: i32) {
+        self.x += dx;
+        self.y += dy;
+    }
+
+    pub fn draw(&self, con: &mut Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
+    }
+
+    pub fn clear(&self, con: &mut Console) {
+        con.put_char(self.x, self.y, ' ', BackgroundFlag::None);
+    }
+}
+
+fn handle_keys(root: &mut Root, player: &mut Object) -> bool {
     let key = root.wait_for_keypress(true);
     match key {
         Key { code: Enter, alt: true, ..} => {
@@ -18,10 +51,10 @@ fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool 
         },
         Key { code: Escape, .. } => return true,
 
-        Key { code: Up, .. } => *player_y -= 1,
-        Key { code: Down, .. } => *player_y += 1,
-        Key { code: Left, .. } => *player_x -= 1,
-        Key { code: Right, .. } => *player_x += 1,
+        Key { code: Up, .. } => player.move_by(0, -1),
+        Key { code: Down, .. } => player.move_by(0, 1),
+        Key { code: Left, .. } => player.move_by(-1, 0),
+        Key { code: Right, .. } => player.move_by(1, 0),
 
         _ => {},
     }
@@ -35,22 +68,32 @@ fn main() {
         .size(SCREEN_WIDTH, SCREEN_HEIGHT)
         .title("Rust/libtcod tutorial")
         .init();
+    let mut con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
 
     tcod::system::set_fps(LIMIT_FPS);
 
-    let mut player_x = SCREEN_WIDTH / 2;
-    let mut player_y = SCREEN_HEIGHT / 2;
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', colors::WHITE);
+    let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', colors::YELLOW);
+    let mut objects = [player, npc];
 
     while !root.window_closed() {
-        root.set_default_foreground(colors::WHITE);
+        con.set_default_foreground(colors::WHITE);
 
-        root.put_char(player_x, player_y, ' ', BackgroundFlag::None);
-        // handle keys and exit game if needed
-        let exit = handle_keys(&mut root, &mut player_x, &mut player_y);
+        for object in &objects {
+            object.draw(&mut con);
+        }
+
+        blit(&mut con, (0, 0), (SCREEN_WIDTH, SCREEN_HEIGHT), &mut root, (0, 0), 1.0, 1.0);
+        root.flush();
+        
+        for object in &objects {
+            object.clear(&mut con);
+        }
+
+        let player = &mut objects[0];
+        let exit = handle_keys(&mut root, player);
         if exit {
             break
         }
-        root.put_char(player_x, player_y, '@', BackgroundFlag::None);
-        root.flush();
     }
 }
